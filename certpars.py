7931d7eb89b9e755b55c1ea5b91886a2fdb5b9f7
SavannahVAna <99336810+SavannahVAna@users.noreply.github.com>
2022-10-10 19:11:35 +0200

    changes in names + requirements

--- certpars.py
+++ certpars.py
@@ -1,5 +1,8 @@
 import os
 from cryptography import x509
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives.asymmetric.rsa import RSAPublicKey
+from cryptography.hazmat.primitives.asymmetric.ec import ECDSA
 
 def parser():
     path = "basic_certs"
@@ -7,4 +10,26 @@ def parser():
         with open(path + "\\" + filename, 'rb') as f:
             m = f.read()
             cer = x509.load_pem_x509_certificate(m)
-            print("certificate validity from " + str(cer.not_valid_before) + " to " + str(cer.not_valid_after)+ "\ncertificate id : " + str(cer.signature_algorithm_oid))
+            print("certificate validity from " + str(cer.not_valid_before) + " to " + str(cer.not_valid_after)+ "\nbeginning verification:")
+            pubk = cer.public_key()
+        if(isinstance(pubk, RSAPublicKey)):
+            try:
+                pubk.verify(
+                    cer.signature,
+                    cer.tbs_certificate_bytes,
+                    padding.PKCS1v15(),
+                    cer.signature_hash_algorithm
+                )
+                print("cryptographic validity successfully proven")
+            except:   
+                print("couldn't verify cerificate") 
+        else:
+            try:
+                pubk.verify(
+                    cer.signature,
+                    cer.tbs_certificate_bytes,
+                    ECDSA(cer.signature_hash_algorithm)
+                )
+                print("cryptographic validity successfully proven")
+            except:   
+                print("couldn't verify cerificate") 
--- readme.md
+++ readme.md
@@ -4,13 +4,13 @@ This python project is made to create X509 certificates and parse them to assert
 
 ## Project composition
 This project is composed of 4 scripts :
--base.py controls the 3 other scripts
--mainf.py creates the root certificate authority and registration authority
--deuetap.py lets you create certificates with chosen cipher and signature algorithms
+-main.py controls the 3 other scripts
+-createcert.py creates the root certificate authority and registration authority
+-rootcertmaker.py lets you create certificates with chosen cipher and signature algorithms
 -certpars.py parses the generated certificates
 
 ## Usage
-Provided that you have installed the cryptography library, you can just launch base.py for the project to work.
+Provided that you have installed the cryptography library, you can just launch main.py for the project to work.
 It will automatically create a root certificate and registration authority.
 Then you will be asked if you want to create certificates and you will be asked which signing and cipher algorithm to use, and put the created cerificates in basic_certs folder.
-Finally it will parse the certificates in the basic_certs folder and display their ID and validity period. 
+Finally it will parse the certificates in the basic_certs folder and display their validity period and assert their cryptographic validity. 
